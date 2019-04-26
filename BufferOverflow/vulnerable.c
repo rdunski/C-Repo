e6befe507c0270ba75536ec563eef32b894d578f
Robert Dunski <rdunski411@gmail.com>
2019-04-26 13:54:31 -0600

    Buffer Overflow init

---
+++ BufferOverflow/vulnerable.c
@@ -0,0 +1,10 @@
+// Vulnerable program
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+	char buffer[100];
+	strcpy(buffer, argv[1]);
+	printf("Done!\n");
+	return 0;
+}
